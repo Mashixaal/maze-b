247df74f3c9aa984be0ee14a96c9f817e324a49f
mashixaal <safwan7342@gmail.com>
2022-10-27 11:35:57 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,65 @@
+// import _ from 'lodash';
+// import './style.css';
+// import Icon from './icon.png/opti.png';
+import "@fortawesome/fontawesome-free/js/all";
+import showDate from './components/dailyDate';
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
+// function component() {
+//     const element = document.createElement('div');
+
+//     // Lodash, currently included via a script, is required for this line to work
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+//      // Add the image to our existing div.
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
+
+//   element.appendChild(myIcon);
+//     return element;
+//   }
+
+//   document.body.appendChild(component());
+
+// console.log("script ok");
+
+// let button = document.getElementById("dark-mode");
+// let body = document.querySelector("body");
+
+// function darkMode() {
+//    body.classList.toggle("bg-black");
+//    body.classList.toggle("text-white");
+// }
+
+// button.addEventListener("click", darkMode);
+
+const accountNumber = document.getElementById("input-deposite");
+const btn = document.getElementById("submit-number");
+const resultAccount = document.getElementById("result-account");
+const sold = document.getElementById("solde");
+const withdrawSold2 = document.getElementById("input-withdraw");
+const button = document.getElementById("submit-number2");
+
+let solde = 0;
+
+const submit = () => {
+  let results = parseInt(accountNumber.value);
+  let total = (solde += results);
+  sold.textContent = total;
+};
+
+btn.addEventListener("click", submit);
+
+const withdrawSold = () => {
+  let value = parseInt(withdrawSold2.value);
+  let total = (solde -= value);
+  sold.innerHTML = total;
+};
+
+button.addEventListener("click", withdrawSold);
+withdrawSold2.addEventListener("keydown", (key) => {
+  console.log(key);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "custom1": "#78dcca",
+      customYellow: {
+        "yellowlight": "#FFDD00",
+        "yellowdark": "#9B8806",
+      },
+
+      customBlue: {
+        "blue": "#B2F8FC",
+      },
+
+      customGreen: {
+        "green": "#DFF515",
+      },
+
+      customGreenNormal: {
+        "greennormal": "#3FFD00",
+      },
+
+      customElectric: {
+        "electric": "#16F5C5",
+      },
+      
+      red: colors.red,
+      gray: colors.gray,
+      orange: colors.orange,
+      lime: colors.lime,
+      yellow:colors.yellow,
+      green:colors.green,
+      blue:colors.blue,
+      
+    },
+    extend: {
+      backgroundImage: {
+        'maze-bank': "url('../img/money.jpg')",
+        'maze-bank2': "url('../img/maze-bank.jpg')",
+        'money-body': "url('../img/money-body.jpg')",
+        'bank-body': "url('../img/bank-body.jpg')",
+        
+      }
+    },
+  },
+  plugins: [],
+}
